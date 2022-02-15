20fa4029b3ca271e9ff2ce1c9c86ebf4b7dca2bc
Michael Hsieh <mdhsieh8@gmail.com>
2022-02-15 11:26:08 -0800

    refactor: try explicit get request.

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
-# Machine learning model server
-# Need to be named app.py or wsgi.py to automatically be run
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello_world():
-# 	return "<p>Hello, World again!</p>"
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+# By default Flask uses get, same as
+@app.route('/', methods=['GET'])
+# @app.route('/')
 def index():
-    return "Hello, this is flask server welcome message."
+    return "Hello, this is flask server welcome message again."
 
 if __name__ == "__main__":
     app.run()
